a6aa838d4e3dc26da31451a3bd60b89451149f88
Matthieu Mourisson <mareek@gmail.com>
2015-02-24 22:34:26 +0100

    First commit : Show random word in list

---
+++ AideALaLecture/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AideALaLecture
+{
+	public sealed partial class MainPage : Page
+	{
+		private string[] _listeMots = { "elle", "dans", "que", "c'est", "est" };
+
+		private string _motCourant;
+
+		private Random _rnd = new Random();
+
+		public MainPage()
+		{
+			InitializeComponent();
+			ChangeMot();
+		}
+
+		private void ChangeMot()
+		{
+			int i;
+			do
+			{
+				i = _rnd.Next(_listeMots.Length);
+			}
+			while (_listeMots[i] == _motCourant);
+			_motCourant = _listeMots[i];
+			MotLabel.Text = _motCourant;
+        }
+	}
+}
